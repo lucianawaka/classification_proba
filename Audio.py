80038d8703579d33e4505e548877261493fbc7f5
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2022-12-25 19:18:34 -0300

    Filenotfound error

--- Audio.py
+++ Audio.py
@@ -13,23 +13,29 @@ class Audio:
         self.text = ""
     
     def write_audio_to_mp3(self):
-        st.audio(self.audio_bytes, format="audio/wav")
-        # To save audio to a file:
-        wav_file = open(self.audio_name, "wb")
-        wav_file.write(self.audio_bytes)
-    
+        try:
+            st.audio(self.audio_bytes, format="audio/wav")
+            # To save audio to a file:
+            wav_file = open(self.audio_name, "wb")
+            wav_file.write(self.audio_bytes)
+        except FileNotFoundError:
+            st.write("Erro ao salvar o arquivo")
+            
     def get_audio_return_text(self):
         # open the file
         filename = self.audio_name
         r = sr.Recognizer()
     
         with sr.AudioFile(filename) as source:
-        # listen for the data (load audio to memory)
-            audio_data = r.record(source)
-            # recognize (convert from speech to text)
-            text = r.recognize_google(audio_data, language='pt-BR')
-            self.text = text 
-        
+            # listen for the data (load audio to memory)
+            try:
+                audio_data = r.record(source)
+                # recognize (convert from speech to text)
+                text = r.recognize_google(audio_data, language='pt-BR')
+                self.text = text 
+            except FileNotFoundError:
+                st.write("Erro ao converter o áudio")
+            
     def translate_audio(self):
         translator = Translator()
         translation = translator.translate(self.text, dest='en')
